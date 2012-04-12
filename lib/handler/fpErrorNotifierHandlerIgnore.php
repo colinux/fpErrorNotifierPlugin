6f62c3ac5f44582e0beafb9fd16621ebecd013c7
66Ton99 <Forma-PRO@66ton99.org.ua>
2012-04-12 11:00:21 +0300

    Fixed cache path

--- lib/handler/fpErrorNotifierHandlerIgnore.php
+++ lib/handler/fpErrorNotifierHandlerIgnore.php
@@ -3,15 +3,15 @@
 /**
  *
  * @package    fpErrorNotifier
- * @subpackage handler 
- * 
+ * @subpackage handler
+ *
  * @author     Maksim Kotlyar <mkotlar@ukr.net>
  * @author     Ton Sharp <Forma-PRO@66Ton99.org.ua>
  */
 class fpErrorNotifierHandlerIgnore extends fpErrorNotifierHandler
-{ 
+{
   /**
-   * 
+   *
    * @var array
    */
   protected $options = array(
@@ -21,19 +21,19 @@ class fpErrorNotifierHandlerIgnore extends fpErrorNotifierHandler
     'ignore_duplication' => false,
     'ignore_duplication_time' => 3600,
     'log_ignored' => true);
-    
+
   /**
-   * 
+   *
    * @return void
    */
   public function initialize()
-  { 
+  {
     // Prevent blocking of error reporting, becuse of @ - error-control operator.
     if ($this->options['ignore_@'] && 0 == error_reporting()) error_reporting(-2);
-    
+
     return parent::initialize();
   }
-  
+
   /**
    * (non-PHPdoc)
    * @see fpErrorNotifierHandler::handleError()
@@ -42,14 +42,14 @@ class fpErrorNotifierHandlerIgnore extends fpErrorNotifierHandler
   {
     // Set becvause of @ error-control operator.
     if ($this->options['ignore_@'] && 0 == error_reporting()) return;
-    
+
     return parent::handleError($errno, $errstr, $errfile, $errline);
   }
-  
+
   /**
-   * 
+   *
    * @param Exception $e
-   * 
+   *
    * @return void
    */
   public function handleException(Exception $e)
@@ -57,12 +57,12 @@ class fpErrorNotifierHandlerIgnore extends fpErrorNotifierHandler
     if ($this->ignoreException($e) || $this->ignoreError($e) || $this->ignoreDuplication($e)) return;
 
     $this->registerExceptionAsKnown($e);
-    
+
     parent::handleException($e);
   }
-  
+
   /**
-   * 
+   *
    *
    * @param Exception $e
    *
@@ -75,36 +75,36 @@ class fpErrorNotifierHandlerIgnore extends fpErrorNotifierHandler
       $code = $e->getSeverity();
     }
     if (in_array($code, $this->options['ignore_errors'])) {
-      $this->logIgnored($e);      
+      $this->logIgnored($e);
       return true;
     }
-   
+
     return false;
   }
-  
+
   /**
    *
    * @param Exception $e
-   * 
+   *
    * @return boolean
    */
   protected function ignoreDuplication(Exception $e)
   {
     if (false == $this->options['ignore_duplication']) return false;
-    
+
     $key = md5($e->getMessage().$e->getFile().$e->getLine());
     if ($this->getExceptionRegister()->has($key)) {
       $this->logIgnored($e);
       return true;
     }
-    
+
     return false;
   }
-  
+
   /**
    *
-   * @param Exception $e 
-   * 
+   * @param Exception $e
+   *
    * @return void
    */
   protected function registerExceptionAsKnown(Exception $e)
@@ -114,33 +114,31 @@ class fpErrorNotifierHandlerIgnore extends fpErrorNotifierHandler
       $this->getExceptionRegister()->set($key, 1, $this->options['ignore_duplication_time']);
     }
   }
-  
+
   /**
-   * 
+   *
    * @return sfFileCache
    */
   protected function getExceptionRegister()
   {
-    $cacheDir = sfConfig::get('sf_cache_dir')?
-                  sfConfig::get('sf_cache_dir'):
-                  realpath(dirname(__FILE__) . '/../../../cache');
+    $cacheDir = sfConfig::get('sf_cache_dir', realpath(dirname(__FILE__) . '/../../../../cache'));
     $cacheDir .= '/fpErrorNotifierPlugin';
-    
+
     return new sfFileCache(array('cache_dir' => $cacheDir));
   }
-  
+
   protected function logIgnored(Exception $e)
   {
     if (!$this->options['log_ignored']) return;
-    
+
     $this->notifier()->context()->getLogger()->info(
       'fpErrorNotifierPlugin: Ignored exception `'.get_class($e).'`. Message `'.$e->getMessage() .
-      '`. File `'.$e->getFile().'`. Line `'.$e->getLine().'`'); 
+      '`. File `'.$e->getFile().'`. Line `'.$e->getLine().'`');
   }
-  
+
   /**
-   * Add or remore errors from ignore list on the fly 
-   * 
+   * Add or remore errors from ignore list on the fly
+   *
    * @param int $code
    * @param bool $status - true - put, false - remove error from list
    */
@@ -152,16 +150,16 @@ class fpErrorNotifierHandlerIgnore extends fpErrorNotifierHandler
       unset($this->options['ignore_errors'][array_search($code, $this->options['ignore_errors'])]);
     }
   }
-  
+
   /**
-   * 
+   *
    *
    * @param Exception $e
    *
    * @return
    */
   protected function ignoreException(Exception $e)
-  {    
+  {
     foreach ($this->options['ignore_exceptions'] as $ignoreClass) {
       if ($e instanceof $ignoreClass) {
         $this->logIgnored($e);
