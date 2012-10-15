efdff3444045dd6c1fdf53ec9d60d8d01a405ad0
Colin Darie <colindarie@gmail.com>
2012-10-15 16:44:39 +0200

    Do not use the internal getTraceAsString() anymore to not truncate arguments

--- lib/message/fpErrorNotifierMessageHelper.php
+++ lib/message/fpErrorNotifierMessageHelper.php
@@ -22,8 +22,49 @@ class fpErrorNotifierMessageHelper
       'class' => $e instanceof ErrorException?fpErrorNotifierErrorCode::getName($e->getSeverity()):get_class($e), 
       'code' => $e->getCode(), 'severity' => $e instanceof ErrorException?$e->getSeverity():'null', 
       'message' => $e->getMessage(), 'file' => "File: {$e->getFile()}, Line: {$e->getLine()}", 
-      'trace' => $e->getTraceAsString());
+      'trace' => $this->getExceptionTraceAsString($e));
   }
+
+
+  // non-truncated exception trace from https://gist.github.com/1437966
+  public function getExceptionTraceAsString(Exception $e)
+  {
+    $rtn = "";
+    $count = 0;
+    foreach ($e->getTrace() as $frame) {
+        $args = "";
+        if (isset($frame['args'])) {
+            $args = array();
+            foreach ($frame['args'] as $arg) {
+                if (is_string($arg)) {
+                    $args[] = "'" . $arg . "'";
+                } elseif (is_array($arg)) {
+                    $args[] = "Array";
+                } elseif (is_null($arg)) {
+                    $args[] = 'NULL';
+                } elseif (is_bool($arg)) {
+                    $args[] = ($arg) ? "true" : "false";
+                } elseif (is_object($arg)) {
+                    $args[] = get_class($arg);
+                } elseif (is_resource($arg)) {
+                    $args[] = get_resource_type($arg);
+                } else {
+                    $args[] = $arg;
+                }
+            }
+            $args = join(", ", $args);
+        }
+        $rtn .= sprintf( "#%s %s(%s): %s(%s)\n",
+                                 $count,
+                                 isset($frame['file']) ? $frame['file'] : 'unknown file',
+                                 isset($frame['line']) ? $frame['line'] : 'unknown line',
+                                 (isset($frame['class'])) ? $frame['class'].$frame['type'].$frame['function'] : $frame['function'],
+                                 $args );
+        $count++;
+    }
+    return $rtn;
+  }
+
 
   /**
    * 
